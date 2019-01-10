11faf48f4216b5fef6c6d2631ded12f1937d5357
jiashuai333 <j947627613@163.com>
2019-01-10 09:22:48 +0000

    Initial commit

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    public function home()
+        {
+    return ' '; }
+        public function help()
+        {
+    return ' '; }
+        public function about()
+        {
+    return ' ';}
+}
+
