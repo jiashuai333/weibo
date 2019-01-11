1cb889e13bd6cbabec572c823d325b40383603d0
jiashuai333 <j947627613@163.com>
2019-01-11 03:12:48 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,15 @@
 
 @extends('layouts.default')
-@section('title', ' 主页 ')
 @section('content')
-    <h1>  主页  </h1>
-@stop
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是<a href="https://www.baidu.com/" role="button">百度</a>
+        </p>
+        <p>
+            一切，将从这里开始
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div> @stop
