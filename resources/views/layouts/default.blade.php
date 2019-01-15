03621190a0e30c86ac277af291f961850c78789e
jiashuai333 <j947627613@163.com>
2019-01-15 09:09:12 +0000

    用户注册

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,5 +1,4 @@
 <html>
-
     <head>
         <title>@yield('title','WeiBo App') - The First Time TO Do It!</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
@@ -9,6 +8,7 @@
         @include('layouts._header')
         <div  class="container">
             <div style="margin-top: 10px" class="offset-md-1 col-md-10">
+                @include('shared._messages')
                 @yield('content')
                 @include('layouts._footer')
             </div>
